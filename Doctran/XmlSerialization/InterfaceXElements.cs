7315684a61197362d64fad4e857c73de33a2c34c
CPardi <chris_pardi@yahoo.co.uk>
2016-02-13 13:29:46 +0000

    Commented constructors for InterfaceXElements.cs

--- Doctran/XmlSerialization/InterfaceXElements.cs
+++ Doctran/XmlSerialization/InterfaceXElements.cs
@@ -11,15 +11,52 @@ namespace Doctran.XmlSerialization
     using System.Collections.Generic;
     using System.Xml.Linq;
     using Parsing;
+    using Utilitys;
 
     public class InterfaceXElements<TParsed> : IInterfaceXElements, IInterfaceXElements<TParsed>
         where TParsed : IFortranObject
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="InterfaceXElements{TParsed}" /> class.
+        ///     Specifies that an interface should generate a single node with the specified name and value.
+        /// </summary>
+        /// <param name="name">The XML node's name.</param>
+        /// <param name="getValue">The XML node's value.</param>
+        public InterfaceXElements(string name, Func<TParsed, string> getValue)
+            : this(name, getValue, parsed => true)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="InterfaceXElements{TParsed}" /> class.
+        ///     Specifies that an interface should only generate a single node with the specified name and value, when a predicate
+        ///     is true.
+        /// </summary>
+        /// <param name="name">The XML node's name.</param>
+        /// <param name="getValue">The XML node's value.</param>
+        /// <param name="predicate">If this condition is met, then the specified XML will be generated.</param>
+        public InterfaceXElements(string name, Func<TParsed, string> getValue, Predicate<TParsed> predicate)
+            : this(from => CollectionUtils.Singlet(new XElement(name, getValue(from))), predicate)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="InterfaceXElements{TParsed}" /> class.
+        ///     Specifies that an interface should generate an enumeration of nodes as specified by <paramref name="func" />.
+        /// </summary>
+        /// <param name="func">Maps an interface to an enumeration of XML nodes.</param>
         public InterfaceXElements(Func<TParsed, IEnumerable<XElement>> func)
             : this(func, parsed => true)
         {
         }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="InterfaceXElements{TParsed}" /> class.
+        ///     Specifies that an interface should generate an enumeration of nodes as specified by <paramref name="func" />, when
+        ///     a predicate is true.
+        /// </summary>
+        /// <param name="func">Maps an interface to an enumeration of XML nodes.</param>
+        /// <param name="predicate">If this condition is met, then the specified XML will be generated.</param>
         public InterfaceXElements(Func<TParsed, IEnumerable<XElement>> func, Predicate<TParsed> predicate)
         {
             if (!typeof(TParsed).IsInterface)
